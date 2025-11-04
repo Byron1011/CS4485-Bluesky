a81964863ac2e818e5e451a212114ecc3d7c8e9a
tort00 <143570190+tort00@users.noreply.github.com>
2025-11-03 21:55:19 -0600

    11/3 updated auth pages and analytics graph

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,23 +1,27 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { useAuth } from '../AuthContext';
 import logoUrl from '../assets/logo.png';
 
 export default function Topbar() {
   const loc = useLocation();
+  const { user, logout } = useAuth();
+  const nav = useNavigate();
   
   return (
     <header className="topbar">
       <div className= "topbar-inner">
       <div className="topbar-left">
-        {/* Logo */}
+        {/* left Logo and name */}
         <img src={logoUrl} alt="BlueSky Crisis Intel logo" className="logo-img" />
         {/*Title*/}
         <div className="brand">BlueSky Crisis Intel</div>
       </div>
 
+      {/* center main navigation */}
       <nav className="topbar-nav">
         <NavLink
-          to="/"
+          to="/dashboard"
           end
           className={({ isActive }) => `toplink ${isActive ? 'active' : ''}`}
         >
@@ -31,10 +35,49 @@ export default function Topbar() {
         </NavLink>
       </nav>
 
+      {/* right: user login/out register */}
       <div className="topbar-right">
-        {/* right side controls */}
+        {user ? (
+            <>
+              <Link
+                to="/user"
+                className="btn btn--md btn--ghost"
+                title="User page"
+              >
+                {user.username}
+              </Link>
+
+              <button
+                type="button"
+                className="btn btn--md btn--primary"
+                onClick={async () => {
+                  await logout();
+                  nav('/login');
+                }}
+                title="Logout"
+              >
+                Logout
+              </button>
+
+              {user.role !== 'guest' && (
+                <Link to="/protected" className="btn btn--md btn--ghost">
+                  Protected
+                </Link>
+              )}
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="btn btn--md btn--ghost">
+                Login
+              </Link>
+              <Link to="/register" className="btn btn--md btn--primary">
+                Register
+              </Link>
+            </>
+          )}
       </div>
       </div>
     </header>
   );
 }
+
