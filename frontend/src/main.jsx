bc57009714e53bd279188f1330db06f650655c06
tort00 <143570190+tort00@users.noreply.github.com>
2025-11-17 22:21:23 -0600

    added notifcations. overall bugfixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import { AuthProvider } from './AuthContext'
-import Root from './Root'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { AuthProvider } from './AuthContext';
+import Root from './Root';
+
+import { NotificationProvider } from './NotificationContext';
+import Notifications from './Notifications';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <Root />
-      </BrowserRouter>
+      <NotificationProvider>
+        <BrowserRouter>
+          <Notifications />
+          <Root />
+        </BrowserRouter>
+      </NotificationProvider>
     </AuthProvider>
   </StrictMode>,
-)
+);
